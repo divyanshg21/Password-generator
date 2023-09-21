4ddb72972e9a7ac61e43306a3d08a81d18f75273
[Divyansh Gupta] <[divyanshgupta2003@gmail.com]>
2023-09-22 01:17:24 +0530

    easy_version_final

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input(f"How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
@@ -16,4 +16,31 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-# test for branch
+pass_letters=[]
+for i in range(0,nr_letters):
+    random_letter = random.randint(0,len(letters)-1)
+    pass_letters.append(letters[random_letter])
+# print(pass_letters)
+
+
+pass_symbols=[]
+for i in range(0,nr_symbols):
+   random_symbol = random.randint(0,len(symbols)-1)
+   pass_symbols.append(symbols[random_symbol])
+# print(pass_symbols)
+
+
+pass_numbers=[]
+for i in range(0,nr_numbers):
+    random_number = random.randint(0,len(numbers)-1)
+    pass_numbers.append(numbers[random_number])
+# print(pass_numbers)
+
+
+final_password=pass_letters+pass_symbols+pass_numbers
+print(f"Generated password:")
+print(*final_password,sep='')
+      
+
+
+
